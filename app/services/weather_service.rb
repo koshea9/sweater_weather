3c7b28bcc821092e7e79a62f834b0b8a03f56975
koshea9 <keegan.oshea9@gmail.com>
2021-06-12 13:17:22 -0600

    add service call to get weather based on location

---
+++ app/services/weather_service.rb
@@ -0,0 +1,24 @@
+class WeatherService
+  def self.get_weather_data(lat, lon)
+    response = conn.get('/data/2.5/onecall') do |req|
+      req.params['lat'] = lat
+      req.params['lon'] = lon
+      req.params['exclude'] = 'minutely'
+    end
+    parse_json(response)
+  end
+
+  private
+  def self.conn
+    Faraday.new(
+      url: 'https://api.openweathermap.org',
+      params: {
+        appid: ENV['open_weather_api_key']
+      }
+    )
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe WeatherService do
   it 'returns weather data given valide latitude and longitude paramters' do
     VCR.use_cassette('Denver weather') do
       lat = 39.738453
-      lng = -104.984853
-      response = WeatherService.get_weather_data(lat, lng)
+      lon = -104.984853
+      response = WeatherService.get_weather_data(lat, lon)
 
       expect(response).to be_a(Hash)
+      expect(response).to_not have_key(:minutely)
       # current_weather
       expect(response).to have_key(:current)
       expect(response[:current]).to be_a(Hash)
@@ -37,27 +38,27 @@ RSpec.describe WeatherService do
       expect(response[:current][:visibility]).to be_a(Integer)
       # conditions
       expect(response[:current]).to have_key(:weather)
-      expect(response[:current][:weather]).to be_a(Hash)
-      expect(response[:current][:weather]).to have_key(:description)
-      expect(response[:current][:weather][:description]).to be_a(String)
+      expect(response[:current][:weather]).to be_a(Array)
+      expect(response[:current][:weather][0]).to have_key(:description)
+      expect(response[:current][:weather][0][:description]).to be_a(String)
       # icon
-      expect(response[:current][:weather]).to have_key(:icon)
-      expect(response[:current][:weather][:icon]).to be_a(String)
+      expect(response[:current][:weather][0]).to have_key(:icon)
+      expect(response[:current][:weather][0][:icon]).to be_a(String)
       # daily_weather
       expect(response).to have_key(:daily)
       expect(response[:daily]).to be_a(Array)
       expect(response[:daily].count).to eq(8)
       # datetime
-      expect(response[:daily]).to have_key(:dt)
+      expect(response[:daily][0]).to have_key(:dt)
       expect(response[:daily][0][:dt]).to be_a(Integer)
       # sunrise
-      expect(response[:daily]).to have_key(:sunrise)
+      expect(response[:daily][0]).to have_key(:sunrise)
       expect(response[:daily][0][:sunrise]).to be_a(Integer)
       # sunset
-      expect(response[:daily]).to have_key(:sunset)
+      expect(response[:daily][0]).to have_key(:sunset)
       expect(response[:daily][0][:sunset]).to be_a(Integer)
       # max_temp
-      expect(response[:daily]).to have_key(:temp)
+      expect(response[:daily][0]).to have_key(:temp)
       expect(response[:daily][0][:temp]).to be_a(Hash)
       expect(response[:daily][0][:temp]).to have_key(:min)
       expect(response[:daily][0][:temp][:min]).to be_a(Float)
@@ -65,27 +66,28 @@ RSpec.describe WeatherService do
       expect(response[:daily][0][:temp]).to have_key(:max)
       expect(response[:daily][0][:temp][:max]).to be_a(Float)
       # conditions
-      expect(response[:daily][0][:weather]).to have_key(:description)
-      expect(response[:daily][0][:weather][:description]).to be_a(String)
+      expect(response[:daily][0][:weather][0]).to have_key(:description)
+      expect(response[:daily][0][:weather][0][:description]).to be_a(String)
       # icon
-      expect(response[:daily][0][:weather]).to have_key(:icon)
-      expect(response[:daily][0][:weather][:icon]).to be_a(String)
+      expect(response[:daily][0][:weather][0]).to have_key(:icon)
+      expect(response[:daily][0][:weather][0][:icon]).to be_a(String)
       # hourly_weather
       expect(response).to have_key(:hourly)
       expect(response[:hourly]).to be_a(Array)
       expect(response[:hourly].count).to eq(48)
       # time
-      expect(response[:hourly]).to have_key(:dt)
+      expect(response[:hourly][0]).to have_key(:dt)
       expect(response[:hourly][0][:dt]).to be_a(Integer)
       # temperature
-      expect(response[:hourly]).to have_key(:temp)
+      expect(response[:hourly][0]).to have_key(:temp)
       expect(response[:hourly][0][:temp]).to be_a(Float)
       # conditions
-      expect(response[:hourly][0][:weather]).to have_key(:description)
-      expect(response[:hourly][0][:weather][:description]).to be_a(String)
+      expect(response[:hourly][0][:weather][0]).to have_key(:description)
+      expect(response[:hourly][0][:weather][0][:description]).to be_a(String)
       # icon
-      expect(response[:hourly][0][:weather]).to have_key(:icon)
-      expect(response[:hourly][0][:weather][:icon]).to be_a(String)
+      expect(response[:hourly][0][:weather][0]
+      ).to have_key(:icon)
+      expect(response[:hourly][0][:weather][0][:icon]).to be_a(String)
     end
   end
 end
