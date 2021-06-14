45989850bda83dbe915f365111b312cdbfe83cb1
koshea9 <keegan.oshea9@gmail.com>
2021-06-14 11:47:37 -0600

    fix poro test to use forecast facade

--- spec/poros/books_spec.rb
+++ spec/poros/books_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Books do
-  it 'creates a poro for book with readable attributes' do
+  it 'creates a poro for book with readable attributes', :vcr do
     location = 'denver,co'
     book_read = File.read("spec/fixtures/books_search_fixture.json")
     book_data = JSON.parse(book_read, symbolize_names: true)
 
     quantity = '5'
-    forecast_read = File.read("spec/fixtures/denver_weather_fixture.json")
-    forecast_data = JSON.parse(forecast_read, symbolize_names: true)
+    forecast_data = ForecastsFacade.location_forecast(location)
+
 
     book = Books.new(location, quantity, forecast_data, book_data)
 
