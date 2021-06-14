472c7d5cb41a54ad743bad8bef2db2176e67011d
koshea9 <keegan.oshea9@gmail.com>
2021-06-14 09:31:13 -0600

    WIP book search service

--- app/controllers/api/v1/books_search_controller.rb
+++ app/controllers/api/v1/books_search_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::BooksSearchController < ApplicationController
   def show
+    conn = Faraday.new("http://openlibrary.org/")
+
+    location = "denver,co"
+    response = conn.get("/search.json") do |req|
+      req.params['q'] = location
+      req.params['subject'] = "travel"
+    end
+    book_data = JSON.parse(response.body, symbolize_names: true)
+    require "pry"; binding.pry
   end
 end
