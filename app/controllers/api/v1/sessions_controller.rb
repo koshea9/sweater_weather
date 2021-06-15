685f6a73ad3ab7d30297e3403dccdfff0cbaa947
koshea9 <keegan.oshea9@gmail.com>
2021-06-15 09:49:40 -0600

    add happy path for user login feature

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UsersSerializer.new(user), status: 200
+    else
+      render json: { errors: [{
+        status: '401',
+        detail: "Invalid credentials"
+      }] }, status: 400
+    end
+  end
+end
--- spec/requests/api/v1/user_login_request_spec.rb
+++ spec/requests/api/v1/user_login_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "user login endpoint" do
   describe "happy path" do
     it 'returns the users api_key when they login' do
-      registered_user_1 = User.create!(email: "user1@test.com", password: "passwordtest", api_key: "o-Iwpwaje4D1JCGudAL75Q")
+      registered_user_1 = User.create!(email: "user1@test.com", password: "passwordtest", password_confirmation: "passwordtest")
       user_login = ({
             "email": "user1@test.com",
             "password": "passwordtest"
@@ -12,30 +12,29 @@ RSpec.describe "user login endpoint" do
       post '/api/v1/sessions', headers: headers, params: JSON.generate(user_login)
 
       login_user_data = JSON.parse(response.body, symbolize_names: true)
-
       expect(response).to be_successful
       expect(response.status).to eq(200)
 
-      expect(registered_user_data).to be_a(Hash)
-      expect(registered_user_data).to have_key(:data)
-      expect(registered_user_data[:data]).to be_a(Hash)
+      expect(login_user_data).to be_a(Hash)
+      expect(login_user_data).to have_key(:data)
+      expect(login_user_data[:data]).to be_a(Hash)
 
-      expect(registered_user_data[:data]).to have_key(:type)
-      expect(registered_user_data[:data][:type]).to be_a(String)
+      expect(login_user_data[:data]).to have_key(:type)
+      expect(login_user_data[:data][:type]).to be_a(String)
 
-      expect(registered_user_data[:data]).to have_key(:id)
-      expect(registered_user_data[:data][:id]).to be_a(String)
+      expect(login_user_data[:data]).to have_key(:id)
+      expect(login_user_data[:data][:id]).to be_a(String)
 
-      expect(registered_user_data[:data]).to have_key(:attributes)
-      expect(registered_user_data[:data][:attributes]).to be_a(Hash)
+      expect(login_user_data[:data]).to have_key(:attributes)
+      expect(login_user_data[:data][:attributes]).to be_a(Hash)
 
-      expect(registered_user_data[:data][:attributes]).to have_key(:email)
-      expect(registered_user_data[:data][:attributes][:email]).to be_a(String)
+      expect(login_user_data[:data][:attributes]).to have_key(:email)
+      expect(login_user_data[:data][:attributes][:email]).to be_a(String)
 
-      expect(registered_user_data[:data][:attributes]).to have_key(:api_key)
-      expect(registered_user_data[:data][:attributes][:api_key]).to eq("o-Iwpwaje4D1JCGudAL75Q")
+      expect(login_user_data[:data][:attributes]).to have_key(:api_key)
+      expect(login_user_data[:data][:attributes][:api_key]).to eq(registered_user_1.api_key)
 
-      expect(registered_user_data[:data][:attributes]).to_not have_key(:password)
+      expect(login_user_data[:data][:attributes]).to_not have_key(:password)
     end
   end
 end
