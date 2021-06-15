1b47ce00f4e8baa35dc3aa54a6cc43e361e3d1fd
koshea9 <keegan.oshea9@gmail.com>
2021-06-14 20:16:23 -0600

    fix schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  before_save :generate_api_key
   validates :email, uniqueness: true, presence: true
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :password, presence: { require: true }
-  validates :api_key, uniqueness: true, presence: true
   has_secure_password
+
+  def generate_api_key
+    self.api_key ||= SecureRandom.urlsafe_base64
+  end
 end
